caab36fe67d5ab450321c68e3dfa881790c2e0a4
tivnanmatt <tivnanmatt@gmail.com>
2024-06-28 20:52:56 +0000

    made a command line interface

---
+++ python/app.py
@@ -0,0 +1,5 @@
+def main():
+    print("Welcome to the Laboratory package!")
+
+if __name__ == "__main__":
+    main()
--- python/setup.py
+++ python/setup.py
@@ -9,7 +9,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            # Define command-line scripts here
+            'laboratory=app:main',
         ],
     },
 )
